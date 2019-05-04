ba38ec694d75787211498f672176e515ac9ae746
Matt Eland <Matt.Eland@GMail.com>
2019-05-04 12:22:24 -0400

    Add project files.

---
+++ MattEland.Emergence.WinCore/GameViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MattEland.Emergence.WinCore
+{
+    public class GameViewModel
+    {
+        public GameViewModel()
+        {
+            GameObjects.Add("What");
+            GameObjects.Add("the");
+            GameObjects.Add("Fez?");
+        }
+
+        public ObservableCollection<string> GameObjects { get; } = new ObservableCollection<string>();
+    }
+}
