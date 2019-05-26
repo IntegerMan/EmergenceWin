10090c5c6ff180eec56a8503231670ee661c049a
Matt Eland <Matt.Eland@GMail.com>
2019-05-26 16:25:54 -0400

    Added game manager tests

---
+++ MattEland.Emergence.Tests/GameManagerTests.cs
@@ -0,0 +1,68 @@
+﻿using MattEland.Emergence.Engine;
+using MattEland.Emergence.Engine.Model;
+using MattEland.Emergence.Engine.Model.EngineDefinitions;
+using NUnit.Framework;
+using Shouldly;
+
+namespace MattEland.Emergence.Tests
+{
+    public class GameManagerTests
+    {
+        [Test]
+        public void StartingGameManagerShouldReturnMessages()
+        {
+            // Arrange
+            var manager = new GameManager();
+
+            // Act
+            var messages = manager.Start();
+
+            // Assert
+            messages.ShouldNotBeNull();
+            messages.ShouldNotBeEmpty();
+        }
+
+        [Test]
+        public void StartingGameManagerShouldResultInStartedState()
+        {
+            // Arrange
+            var manager = new GameManager();
+
+            // Act
+            manager.Start();
+
+            // Assert
+            manager.State.ShouldBe(GameStatus.Ready);
+        }
+
+        [Theory]
+        [TestCase(MoveDirection.Up)]
+        [TestCase(MoveDirection.Right)]
+        [TestCase(MoveDirection.Down)]
+        [TestCase(MoveDirection.Left)]
+        public void GameManagerShouldRespondToMovesWithMessages(MoveDirection direction)
+        {
+            // Arrange
+            var manager = new GameManager();
+            manager.Start();
+
+            // Act
+            var messages = manager.MovePlayer(direction);
+
+            // Assert
+            messages.ShouldNotBeNull();
+            messages.ShouldNotBeEmpty();
+        }
+
+        [Test]
+        public void GameManagerShouldStartNotStarted()
+        {
+            // Arrange
+            var manager = new GameManager();
+
+            // Assert
+            manager.State.ShouldBe(GameStatus.NotStarted);
+        }
+
+    }
+}
--- Sonar.bat
+++ Sonar.bat
@@ -1,7 +1,7 @@
 SonarScanner.MSBuild.exe begin /k:"IntegerMan_EmergenceWin" /o:"integerman-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="b7b4509740d666b273996bbc0f691e8b897b80d8" /d:sonar.cs.nunit.reportsPaths="%CD%\NUnitResults.xml" /d:sonar.cs.opencover.reportsPaths="%CD%\opencover.xml"
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MsBuild.exe" /t:Rebuild
 
-OpenCover.Console.exe -output:"%CD%\opencover.xml" -register:user -target:"C:\Users\Matt\.nuget\packages\nunit.consolerunner\3.10.0\tools\nunit3-console.exe" -targetargs:"%CD%\MattEland.Emergence.Tests\bin\Debug\MattEland.Emergence.Tests.dll --result=%CD%\NUnitResults.xml"
+OpenCover.Console.exe -output:"%CD%\opencover.xml" -register:user -target:"nunit3-console.exe" -targetargs:"%CD%\MattEland.Emergence.Tests\bin\Debug\MattEland.Emergence.Tests.dll --result=%CD%\NUnitResults.xml"
 
 SonarScanner.MSBuild.exe end /d:sonar.login="b7b4509740d666b273996bbc0f691e8b897b80d8"
 Pause
