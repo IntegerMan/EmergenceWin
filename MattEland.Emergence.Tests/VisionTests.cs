ea5474c14706e469fe5bbe8b57007abe8e727ca1
Matt Eland <Matt.Eland@GMail.com>
2019-05-26 19:25:49 -0400

    Added basic vision tests

---
+++ MattEland.Emergence.Tests/VisionTests.cs
@@ -0,0 +1,46 @@
+﻿using MattEland.Emergence.Engine.Level;
+using MattEland.Emergence.Engine.Model;
+using NUnit.Framework;
+using Shouldly;
+
+namespace MattEland.Emergence.Tests
+{
+    public class VisionTests : EmergenceTestBase
+    {
+
+        [SetUp]
+        public void Initialize()
+        {
+            base.InitializeGameService();
+
+            Context.ClearMessages();
+        }
+
+        [Test]
+        public void PlayerShouldBeAbleToSeeTheirOwnTile()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Player.VisibleCells.ShouldContain(Player.Pos);
+        }
+
+        [Theory]
+        [TestCase(MoveDirection.Left)]
+        [TestCase(MoveDirection.Up)]
+        [TestCase(MoveDirection.Down)]
+        [TestCase(MoveDirection.Right)]
+        public void PlayerShouldBeAbleToSeeNeighboringTiles(MoveDirection direction)
+        {
+            // Arrange
+            var target = Player.Pos.GetNeighbor(direction);
+
+            // Act
+
+            // Assert
+            Player.VisibleCells.ShouldContain(target);
+        }
+    }
+}
