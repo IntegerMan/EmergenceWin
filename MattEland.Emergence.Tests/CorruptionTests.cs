e85dabbc0c8014908b90cd7acd4a2238acb34c66
Matt Eland <Matt.Eland@GMail.com>
2019-05-26 20:22:28 -0400

    Added corruption test

---
+++ MattEland.Emergence.Tests/CorruptionTests.cs
@@ -0,0 +1,33 @@
+﻿using MattEland.Emergence.Engine.Level;
+using MattEland.Emergence.Engine.Model;
+using MattEland.Emergence.Engine.Services;
+using NUnit.Framework;
+using Shouldly;
+
+namespace MattEland.Emergence.Tests
+{
+    public class CorruptionTests : EmergenceTestBase
+    {
+        [SetUp]
+        public void Initialize()
+        {
+            base.InitializeGameService();
+
+            Context.ClearMessages();
+        }
+
+        [Test]
+        public void CorruptionDamageShouldCauseCorruption()
+        {
+            // Arrange
+            var turret = CreateTurret(Player.Pos.GetNeighbor(MoveDirection.Left));
+
+            // Act
+            Context.CombatManager.HurtObject(Context, Player, turret, 1, "Corruption", DamageType.Corruption);
+
+            // Assert
+            turret.Corruption.ShouldBeGreaterThan(0);
+        }
+        
+    }
+}
