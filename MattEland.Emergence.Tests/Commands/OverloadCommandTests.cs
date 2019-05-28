ce5360276107a0affbc2679e2f1c0956ec19d80e
Matt Eland <Matt.Eland@GMail.com>
2019-05-28 15:27:21 -0400

    Added tests around wall creation logic and Overload command.

---
+++ MattEland.Emergence.Tests/Commands/OverloadCommandTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using MattEland.Emergence.Engine.Commands;
+using NUnit.Framework;
+using Shouldly;
+
+namespace MattEland.Emergence.Tests.Commands
+{
+    public class OverloadCommandTests : EmergenceTestBase
+    {
+
+        [Test]
+        public void OverloadShouldDamageNearbyEntities()
+        {
+            // Arrange
+            var potentialTargets = Context.Level.Objects.Where(o => !o.IsPlayer && !o.IsInvulnerable).ToList();
+            int originalDamage = potentialTargets.Sum(o => o.MaxStability - o.Stability);
+
+            // Act
+            GameService.HandleCommand(new OverloadCommand(), Player.Pos);
+
+            // Assert
+            potentialTargets.Sum(o => o.MaxStability - o.Stability).ShouldBeGreaterThan(originalDamage);
+        }
+
+        [Test]
+        public void OverloadShouldDamageExecutingEntity()
+        {
+            // Arrange
+            int originalStability = Player.Stability;
+            Player.EffectiveDefense = 0;
+
+            // Act
+            GameService.HandleCommand(new OverloadCommand(), Player.Pos);
+
+            // Assert
+            Player.Stability.ShouldBeLessThan(originalStability);
+        }
+    }
+}
---
+++ MattEland.Emergence.Tests/WallTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MattEland.Emergence.Engine.Entities;
+using MattEland.Emergence.Engine.Model;
+using MattEland.Emergence.Engine.Services;
+using NUnit.Framework;
+using Shouldly;
+
+namespace MattEland.Emergence.Tests
+{
+    public class WallTests : EmergenceTestBase
+    {
+
+        [Test]
+        public void DestroyingAWallShouldCreateRubble()
+        {
+            // Arrange
+            var pos = Player.Pos.Add(0, -3);
+            var cell = Context.Level.GetCell(pos);
+            var wall = cell.Objects.OfType<Wall>().Single();
+
+            // Act
+            Context.CombatManager.HurtObject(Context, Player, wall, wall.MaxStability, "Hacks", DamageType.Normal);
+
+            // Assert
+            cell.Objects.OfType<Debris>().Count().ShouldBe(1);
+        }
+
+        [Test]
+        public void DestroyingAWallShouldRemoveIt()
+        {
+            // Arrange
+            var pos = Player.Pos.Add(0, -3);
+            var cell = Context.Level.GetCell(pos);
+            var wall = cell.Objects.OfType<Wall>().Single();
+
+            // Act
+            Context.CombatManager.HurtObject(Context, Player, wall, wall.MaxStability, "Hacks", DamageType.Normal);
+
+            // Assert
+            wall.IsDead.ShouldBeTrue();
+            cell.Objects.ShouldNotContain(wall);
+        }
+
+        [Test]
+        public void DestroyingAWallShouldCreateAdditionalWallsIfNeeded()
+        {
+            // Arrange
+            var pos = Player.Pos.Add(0, -3);
+            var behindPos = pos.GetNeighbor(MoveDirection.Up);
+
+            var cell = Context.Level.GetCell(pos);
+            var wall = cell.Objects.OfType<Wall>().Single();
+
+            var behindCell = Context.Level.GetCell(behindPos);
+            behindCell.ShouldBeNull();
+
+            // Act
+            Context.CombatManager.HurtObject(Context, Player, wall, wall.MaxStability, "Hacks", DamageType.Normal);
+
+            // Assert
+            behindCell = Context.Level.GetCell(behindPos);
+            behindCell.ShouldNotBeNull();
+            behindCell.Objects.OfType<Wall>().Count().ShouldBe(1);
+            
+        }
+    }
+}
