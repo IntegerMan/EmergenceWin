e5063436df05ca6c9416994ff9d818b5fc7e6b6f
Matt Eland <Matt.Eland@GMail.com>
2019-05-28 19:28:44 -0400

    Added GameVM tests

---
+++ MattEland.Emergence.Tests/ViewModels/GameViewModelTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MattEland.Emergence.Engine.Entities;
+using MattEland.Emergence.Engine.Model;
+using MattEland.Emergence.WpfCore.ViewModels;
+using NUnit.Framework;
+using Shouldly;
+
+namespace MattEland.Emergence.Tests.ViewModels
+{
+    public class GameViewModelTests
+    {
+        [Test]
+        public void GameViewModelShouldStartWithProperState()
+        {
+            // Arrange
+
+            // Act
+            var vm = new GameViewModel();
+
+            // Assert
+            vm.WorldObjects.ShouldNotBeEmpty();
+            vm.Commands.ShouldNotBeEmpty();
+            vm.Messages.ShouldNotBeEmpty();
+        }
+
+        [Test]
+        public void GameViewModelShouldHandleCommands()
+        {
+            // Arrange
+            var vm = new GameViewModel();
+            var player = vm.WorldObjects.First(o => o.Source.IsPlayer);
+            var oldX = player.X;
+            var oldPos = player.Source.Pos;
+
+            // Act
+            vm.MovePlayer(MoveDirection.Left);
+
+            // Assert
+            player.X.ShouldBe(oldX); // Don't de-center it
+            player.Source.Pos.ShouldBe(oldPos.GetNeighbor(MoveDirection.Left));
+        }
+    }
+}
