5bf6a350684160a764d05c7c3f601737464e785a
Matt Eland <Matt.Eland@GMail.com>
2019-05-31 23:22:34 -0400

    Added App.xaml test for code coverage false positives.

---
+++ MattEland.Emergence.Tests/WpfClientTests.cs
@@ -0,0 +1,12 @@
+﻿using MattEland.Emergence.WpfCore;
+using NUnit.Framework;
+using Shouldly;
+
+namespace MattEland.Emergence.Tests
+{
+    public class WpfClientTests
+    {
+        [Test]
+        public void AppXamlCanBeConstructed() => new App();
+    }
+}
