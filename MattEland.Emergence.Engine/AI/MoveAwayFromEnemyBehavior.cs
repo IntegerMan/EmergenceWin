3132e52e0bc0c40c1390e8655c7e4cb7ad4c3970
Matt Eland <Matt.Eland@GMail.com>
2019-06-23 22:31:51 -0400

    Cleaner cell scoring

--- MattEland.Emergence.Engine/AI/MoveAwayFromEnemyBehavior.cs
+++ MattEland.Emergence.Engine/AI/MoveAwayFromEnemyBehavior.cs
@@ -5,6 +5,7 @@ using MattEland.Emergence.Engine.Entities.Actors;
 using MattEland.Emergence.Engine.Game;
 using MattEland.Emergence.Engine.Level;
 using MattEland.Emergence.Engine.Model;
+using MattEland.Shared.Collections;
 
 namespace MattEland.Emergence.Engine.AI
 {
@@ -47,8 +48,17 @@ namespace MattEland.Emergence.Engine.AI
         /// <param name="context">The game context</param>
         /// <param name="cell">The cell to evaluate</param>
         /// <returns>Gets a numeric score for the cell under evaluation</returns>
-        private decimal ScoreCell(GameContext context, GameCell cell) 
-            => _enemies.Sum(e => e.Pos.CalculateDistanceInMovesFrom(cell.Pos) * 5 + 
-                             cell.FilterAdjacentCells(context, cells => cells.Where(c => !c.HasObstacle)).Count());
+        private decimal ScoreCell(GameContext context, GameCell cell)
+        {
+            var score = 0;
+            
+            // Main criteria is distance from nearest enemy
+            _enemies.Each(e => score += e.Pos.CalculateDistanceInMovesFrom(cell.Pos) * 5);
+
+            // Score the cell by its proximity to openness so the actors will avoid boxing themselves into a corner
+            score += cell.FilterAdjacentCells(context, cells => cells.Where(c => !c.HasObstacle)).Count();
+
+            return score;
+        }
     }
 }
